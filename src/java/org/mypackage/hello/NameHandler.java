bce9f52d6845abe34ccfe850a399f663f5a26db1
Jorge Carrillo <jorgecardi00@gmail.com>
2021-03-22 20:08:19 -0500

    Primera vez subiendo el lab

---
+++ src/java/org/mypackage/hello/NameHandler.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.hello;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+
+/**
+ *
+ * @author jorge_j3qr4sd
+ */
+public class NameHandler {
+    
+    private String name;
+    private int age;
+    private int hora;
+    private String nacimiento;
+    
+    public NameHandler() {
+        name=null;
+        nacimiento=null;
+        hora=Integer.parseInt(DateTimeFormatter.ofPattern("H").format(LocalTime.now()));
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        age=Period.between(LocalDate.parse(nacimiento,DateTimeFormatter.ofPattern("d-M-y")),LocalDate.now()).getYears();
+        return age;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public String getNacimiento() {
+        return nacimiento;
+    }
+
+    public void setNacimiento(String nacimiento) {
+        this.nacimiento = nacimiento;
+    }
+
+    
+    
+    
+}
---
+++ web/response.jsp
@@ -0,0 +1,38 @@
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%-- 
+    Document   : response
+    Created on : 22/03/2021, 04:48:47 PM
+    Author     : jorge_j3qr4sd
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <!-- Nombre -->
+        <jsp:useBean id="mybean" scope="session" class="org.mypackage.hello.NameHandler" />
+        <jsp:setProperty name="mybean" property="name"  />
+        <jsp:setProperty name="mybean" property="nacimiento"  />
+        <h1>Hello, <jsp:getProperty name="mybean" property="name" />!
+        Your age is, <jsp:getProperty name="mybean" property="age" />
+        
+        <c:choose>
+            <c:when test="${mybean.hora>=0 && mybean.hora<12}">
+                , Good morning
+            </c:when>
+            <c:when test="${mybean.hora>=12 && mybean.hora<18}">
+                , Good afternoon
+            </c:when>
+            <c:otherwise>
+                , Good night
+            </c:otherwise>
+        </c:choose>
+        
+        </h1>
+        
+    </body>
+</html>
